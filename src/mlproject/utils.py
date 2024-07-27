0f72369be420c3b6a298c0fbf681bf4e7fe17f5f
Shivaji Mane <maneshiva92@gmail.com>
2024-07-27 20:54:09 +0530

    model training done

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,12 +1,16 @@
-import os
-import sys
-from src.mlproject.exception import CustomException
-from src.mlproject.logger import logging
 import pandas as pd
+import numpy as np
 from dotenv import load_dotenv
 import pymysql
 import pickle
-import numpy as np
+import os
+import sys
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
 
 load_dotenv()
 
@@ -43,5 +47,33 @@ def save_object(file_path, obj):
         with open(file_path, 'wb') as file_obj:
             pickle.dump(obj, file_obj)
             
+    except Exception as e:
+        raise CustomException(sys, e)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
     except Exception as e:
         raise CustomException(sys, e)
